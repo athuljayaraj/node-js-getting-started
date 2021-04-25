87b7559131ee4ee879432f2764969e1a9b7c06c9
Athul S Raj <athul@keyvalue.systems>
2021-04-25 17:32:51 +0530

    Send message to number

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const accountSid = 'ACdcb9bf84f9272441b0a4688fc1f45750';
+const authToken = '8b4034ea9272888ffcb96dd31bc18679';
+const client = require('twilio')(accountSid, authToken);
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .post('/welcome', (req, res) => {
+    client.messages
+      .create({
+        body: 'Your Twilio code is 1238432',
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+919947846480'
+      })
+      .then(message => {
+        console.log(message.sid)
+        res.send(`PMessage sent to ${message}`)
+      }
+      )
+      .done();
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
